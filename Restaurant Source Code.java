169db331fbb214e8539ee379c996f67ebffd9490
Mel <152907429+Melmonster13@users.noreply.github.com>
2024-10-15 18:38:03 -0600

    Add files via upload

---
+++ Restaurant Source Code.java
@@ -0,0 +1,22 @@
+/*
+ * Name: Melanie Cadd
+ * Course: CSC320: Programming I
+ * Assignment: Module 1 CT Option 2
+ * Date: October 13, 2024
+ * Description: This Java program prints the restaurant name, business address, and city, state, and zip code.
+ */
+
+ public class RestaurantInfo {
+    public static void main(String[] args) {
+        // Declare variables and initialize
+        String restaurantName = "Rioja";
+        String businessAddress = "1431 Larimer St";
+        String cityStateZip = "Denver, CO 80202";
+
+        // Output the restaurant's information
+        System.out.println("Restaurant Name: " + restaurantName);
+        System.out.println("Business Address: " + businessAddress);
+        System.out.println("City, State, Zip Code: " + cityStateZip);
+    }
+ }
+ 
